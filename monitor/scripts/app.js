e96b466667dd56a7a04f3067a07a6b7f59ec142a
martin <martin@rga.com>
2015-02-27 12:13:36 -0300

    agrego d3 y circular heat map prototipe

--- monitor/scripts/app.js
+++ monitor/scripts/app.js
@@ -4,6 +4,37 @@ var intro = 'http://api.thingspeak.com/channels/'
 var post = '/feed.json';
 var channels = [28024, 28027, 28029, 28030];
 
+
+//http://prcweb.co.uk/lab/circularheat/
+
+/* Create random data */
+var data = [];
+
+
+var chart = circularHeatChart()
+	.domain([0,44])
+	.range(["rgb(118, 218, 233)","red"])
+	.segmentHeight(10)
+	.innerRadius(8);
+
+var data = [];
+var ringLength = 24;
+var measures = 20;
+var t = 0;
+for (var i= 0; i < measures; i++) {
+	for(var j=0; j<ringLength; j++) {
+		data[j+i*ringLength] = t + i; 
+	}
+
+};
+
+d3.select('#chart2')
+    .selectAll('svg')
+    .data([data])
+    .enter()
+    .append('svg')
+    .call(chart);
+
 //TODO: Reemplazar
 setInterval(function(){
 
@@ -21,6 +52,10 @@ setInterval(function(){
 					var lastIndex = data.feeds.length-1;
 					var lastMeasure = data.feeds[lastIndex];
 					//La aplico en el div.
+
+
+
+
 					$('.spot-' + current + ' h1').html(lastMeasure.field1);
 				}
 		   }).done(function(){
@@ -34,3 +69,8 @@ setInterval(function(){
 	
 
 },4000);
+function randomRange(min, max)
+{
+   var range = (max - min) + 1;     
+   return (Math.random() * range) + min;
+}
